a1e65353d7ca2c15014da1334fcccec9fd607ece
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-05-28 19:50:25 +0100

    Updated footer

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-angular
-  .module('mwl.homepage', ['ui.bootstrap', 'ngAnimate', 'ngTouch', 'duScroll'])
-  .controller('HomepageCtrl', function () {
-    var vm = this;
-    vm.currentDate = new Date();
-  });
+angular.module('mwl.homepage', ['ui.bootstrap', 'ngAnimate', 'ngTouch', 'duScroll']);
