3020538852c970dae254e6a41559f8b77ca39540
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-06-06 00:01:34 +0100

    Add contact form and latest tweets

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-angular.module('mwl.homepage', ['ui.bootstrap', 'ngAnimate', 'ngTouch', 'duScroll']);
+(function(window, angular) {
+
+  angular
+
+    .module('mwl.homepage', [
+      'ui.bootstrap',
+      'ngAnimate',
+      'ngTouch',
+      'ngSanitize',
+      'duScroll'
+    ])
+
+    .constant('apiEndpoint', 'https://api.mattlewis.me')
+
+    .constant('moment', window.moment)
+
+    .controller('ContactFormCtrl', function($http, apiEndpoint) {
+
+      var vm = this;
+
+      vm.submit = function() {
+        vm.loading = true;
+        $http.post(apiEndpoint + '/contact', vm.form).success(function() {
+          vm.submitted = true;
+        }).error(function(error) {
+          vm.error = error;
+        }).finally(function() {
+          vm.loading = false;
+        });
+      };
+
+    })
+
+    .controller('TweetsCtrl', function($http, moment, apiEndpoint) {
+      var vm = this;
+      $http.get(apiEndpoint + '/social/tweets').success(function(tweets) {
+        vm.tweets = tweets.map(function(tweet) {
+          tweet.created_at = moment(tweet.created_at, 'dd MMM DD HH:mm:ss ZZ YYYY', 'en').toDate();
+          return tweet;
+        });
+      });
+    });
+
+}(window, window.angular));
