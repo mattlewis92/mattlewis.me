aa6c260c523cb03fe0c7f14c5e7466899b0fbe58
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2017-03-26 11:16:08 +0100

    More defaults changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'mwl-homepage',
+  template: `
+    <h1>
+      {{ title }}
+    </h1>
+  `
 })
 export class AppComponent {
   title = 'app works!';
