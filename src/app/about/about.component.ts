7a97c1c1010b45be0c8b6e8f20db2b0dbe13aff2
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2017-03-26 14:06:38 +0100

    Auto years

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
 
+const YEAR_STARTED_PROGRAMMING = 2008;
+
 @Component({
   selector: 'mwl-about',
   templateUrl: './about.component.html'
 })
-export class AboutComponent {}
+export class AboutComponent {
+
+  yearsExperience: number = new Date().getFullYear() - YEAR_STARTED_PROGRAMMING;
+
+}
