03cd5c4625ddf15b22fad1498f04aaed97d46835
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2017-03-26 13:26:56 +0100

    Add fastclick polyfill

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as fastclick from 'fastclick';
 
 @Component({
   selector: 'mwl-homepage',
   templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    fastclick.attach(document.body);
+  }
+
+}
