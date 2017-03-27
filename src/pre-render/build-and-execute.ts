b91be640add65c6f646e10dbdc2c2ff977f0f0fe
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2017-03-27 17:26:04 +0100

    Ensure pre-rendering failures fail the build

--- src/pre-render/build-and-execute.ts
+++ src/pre-render/build-and-execute.ts
@@ -8,6 +8,7 @@ compiler.outputFileSystem = new MemoryFileSystem();
 compiler.run((err, stats) => {
   if (err) {
     console.error(err);
+    process.exit(1);
   } else {
     const output = stats.compilation.assets['main.js'].source();
     eval(output); // so gross but the only way I can find currently that doesn't involve writing temporary files to disk
