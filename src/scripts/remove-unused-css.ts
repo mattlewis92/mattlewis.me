dea58f73b8e2f06ef4c594af07d997d9cf565263
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2017-03-29 22:41:43 +0100

    Revert "Include form error classes"
    This reverts commit eb3c42bccca259c1b3998b5e2d59a649d8145501.

--- src/scripts/remove-unused-css.ts
+++ src/scripts/remove-unused-css.ts
@@ -5,6 +5,6 @@ import * as uncss from 'uncss';
 const docsFolder = path.resolve(__dirname, '..', '..', 'docs');
 const cssFileName = fs.readdirSync(docsFolder).filter(file => file.endsWith('.css'))[0];
 
-uncss([path.resolve(docsFolder, 'index.html')], {ignore: [/.*has-error.*/]}, (error, output) => {
+uncss([path.resolve(docsFolder, 'index.html')], (error, output) => {
   fs.writeFileSync(path.resolve(docsFolder, cssFileName), output);
 });
