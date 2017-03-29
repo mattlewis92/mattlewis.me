74c8926c94769293c55423a880f8a8c94a710e3c
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2017-03-29 22:26:52 +0100

    Remove unused css

---
+++ src/scripts/remove-unused-css.ts
@@ -0,0 +1,10 @@
+import * as path from 'path';
+import * as fs from 'fs';
+import * as uncss from 'uncss';
+
+const docsFolder = path.resolve(__dirname, '..', '..', 'docs');
+const cssFileName = fs.readdirSync(docsFolder).filter(file => file.endsWith('.css'))[0];
+
+uncss([path.resolve(docsFolder, 'index.html')], (error, output) => {
+  fs.writeFileSync(path.resolve(docsFolder, cssFileName), output);
+});
