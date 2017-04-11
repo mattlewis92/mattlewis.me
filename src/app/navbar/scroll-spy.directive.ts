1e839fd346fc9244e224a635a7b1ae262738a354
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2017-04-11 12:17:19 +0100

    Less hacky way of detecting if on the browser or server

--- src/app/navbar/scroll-spy.directive.ts
+++ src/app/navbar/scroll-spy.directive.ts
@@ -1,5 +1,6 @@
-import { Directive, HostListener, Input, AfterViewInit, Inject, HostBinding } from '@angular/core';
+import { Directive, HostListener, Input, AfterViewInit, Inject, HostBinding, PLATFORM_ID } from '@angular/core';
 import { DOCUMENT } from '@angular/platform-browser';
+import { isPlatformBrowser } from '@angular/common';
 import { PageScrollConfig } from 'ng2-page-scroll';
 
 function adjustPosition(yCoord: number): number {
@@ -15,7 +16,7 @@ export class ScrollSpyDirective implements AfterViewInit {
 
   @Input('mwlScrollSpy') elementId: string; // tslint:disable-line
 
-  constructor(@Inject(DOCUMENT) private document: any) {}
+  constructor(@Inject(DOCUMENT) private document: any, @Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngAfterViewInit(): void {
     setTimeout(() => this.onScroll());
@@ -24,7 +25,7 @@ export class ScrollSpyDirective implements AfterViewInit {
 
   @HostListener('document:scroll')
   onScroll() {
-    if (typeof this.document.querySelector !== 'undefined') {
+    if (isPlatformBrowser(this.platformId)) {
       const boundingRectangle: ClientRect = this.document.querySelector(`#${this.elementId}`).getBoundingClientRect();
       this.isActive = adjustPosition(boundingRectangle.top) <= 0 && adjustPosition(boundingRectangle.bottom) > 0;
     }
