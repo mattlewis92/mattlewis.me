5150be2e74dcbf8233e9a82deca9d9a4667e117d
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2018-01-27 17:47:59 +0000

    Scroll to section by url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,39 @@
 import { Component, OnInit, PLATFORM_ID, Inject } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
+import {
+  DOCUMENT,
+  HashLocationStrategy,
+  isPlatformBrowser
+} from '@angular/common';
+import { PageScrollInstance, PageScrollService } from 'ngx-page-scroll';
 
 @Component({
   selector: 'mwl-homepage',
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
-  constructor(@Inject(PLATFORM_ID) private platformId: object) {}
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: object,
+    @Inject(DOCUMENT) private document: any,
+    private pageScrollService: PageScrollService
+  ) {}
 
   ngOnInit(): void {
     if (isPlatformBrowser(this.platformId)) {
       System.import('fastclick').then((fastclick) => {
         fastclick.attach(document.body);
       });
+
+      const params = new URLSearchParams(window.location.search);
+      if (params.has('section')) {
+        setTimeout(() => {
+          this.pageScrollService.start(
+            PageScrollInstance.simpleInstance(
+              this.document,
+              `#${params.get('section')}`
+            )
+          );
+        }, 500);
+      }
     }
   }
 }
