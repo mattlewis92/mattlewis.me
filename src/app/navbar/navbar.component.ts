4991839bb62537572c791812e475d4beb6d53b67
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2018-04-09 11:20:36 +0100

    Update email link as recaptcha v1 is shutdown

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -14,12 +14,4 @@ export class NavbarComponent {
   closeMenu(): void {
     this.isMenuVisible = false;
   }
-
-  showEmailCaptcha() {
-    window.open(
-      'https://www.google.com/recaptcha/mailhide/d?k=01ciEon6qhPUtHkA2796MjHQ==&c=ThGegkQW9KQ7Vs-mCVW7AgfavFhM0A417JovGr-JiHc=',
-      '',
-      'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'
-    );
-  }
 }
