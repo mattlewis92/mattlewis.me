a85131b5d52d4c1eaa5f06e415bc2ceb14410925
Matt Lewis <hello@mattlewis.me>
2020-11-21 12:57:56 +0000

    Fix email

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { PageScrollConfig } from 'ngx-page-scroll';
+import { isPlatformBrowser } from '@angular/common';
 
 PageScrollConfig.defaultDuration = 300;
 PageScrollConfig.defaultScrollOffset = 60;
@@ -9,7 +10,13 @@ PageScrollConfig.defaultScrollOffset = 60;
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {
+  email = isPlatformBrowser(this.platformId)
+    ? 'mailto:website@mattlewis.me'
+    : '';
+
   isMenuVisible = false;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: object) {}
 
   closeMenu(): void {
     this.isMenuVisible = false;
